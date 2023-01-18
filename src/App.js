5938e70c1c482a7ed444fb22ac011e00efc1cc35
Michael <mjj.yoyogi@gmail.com>
2023-01-17 23:25:15 -0500

    testing components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
+const App = () => {
+  const [heading, setHeading] = useState("Magnificient Monkeys");
 
-function App() {
+  const clickHandler = () => {
+    setHeading("Radical Rhinos")
+  };
+
   return (
-    
-  );
+    <div>
+      <button type='button' onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </div>
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import {render, screen} from "@testing-library/react"
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+import '@testing-library/jest-dom'
+// import TestComponent from "path-to-test-component"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe("App component", () => {
+  it("renders magnificent monkeys", () =>{
+
+    const {container} = render(<App></App>)
+    expect(container).toMatchSnapshot();
+  });
+
+  it("renders radical rhinos after button clicks", () => {
+    render(<App></App>);
+    const button = screen.getByRole("button", {name: "Click Me"});
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+
+  });
 });
